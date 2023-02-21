6eb1e62637232e6c77062d76c1b32cdd2c67175b
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-02-21 11:13:05 +0900

    로그인 및 메인페이지 레이아웃 체크

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './Login.css'
+import './Login.css';
+
 
 const Login = () => {
     return (
         <main className="login_box">
-            <h1 className='westagram'>Westagram</h1>
+            <h1 className='westagram_login'>Westagram</h1>
             <input id="LoginId" className="login_id" type="name" placeholder=" 전화번호, 사용자이름 또는 이메일" required></input>
             <input id="LoginPw" className="login_pw" type="password" placeholder=" 비밀번호" required></input>
             <button id="LoginBtn" className="login_btn">로그인</button>  

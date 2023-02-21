eafabae61a29556c89e3fde3a5fac4ec45ab0164
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-02-21 15:33:59 +0900

    라우터 수정

--- src/Router.js
+++ src/Router.js
@@ -8,7 +8,7 @@ const Router = () => {
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Login/>} />
-                <Route path='/main' element={<Link />}/>
+                <Route path='/main' element={<Main />}/>
             </Routes>
         </BrowserRouter>
     )
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './Login.css';
+import { useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
+    const navigate = useNavigate();
+
+    const goToMain = () => {
+        navigate('/main');
+    };
+
+
+
     return (
         <main className="login_box">
             <h1 className='westagram_login'>Westagram</h1>
             <input id="LoginId" className="login_id" type="name" placeholder=" 전화번호, 사용자이름 또는 이메일" required></input>
             <input id="LoginPw" className="login_pw" type="password" placeholder=" 비밀번호" required></input>
-            <button id="LoginBtn" className="login_btn">로그인</button>  
+            <button id="LoginBtn" className="login_btn" onClick={goToMain}>로그인</button>
             <a className="forgot_pw" href="https://www.instagram.com/accounts/password/reset/">비밀번호를 잊으셨나요?</a>
         </main>
 )};
